7f8d681a27535b48ebcf965f1f7754a23845d27c
pronamka <defender0508@gmail.com>
2025-04-15 13:49:29 +0700

    added a class to center the image drawn on the canvas, restructured code

--- neural_network/neural_network.js
+++ neural_network/neural_network.js
@@ -10,20 +10,23 @@ canvas.height = Math.floor((window.innerHeight - canvasOffsetY - 300) / 28) * 28
 canvas.width = canvas.height;
 
 let isPainting = false;
-let lineWidth = 65;
+let lineWidth = 60;
 
 canvasContext.lineWidth = lineWidth;
-canvasContext.fillStyle = 'white';
-canvasContext.fillRect(0, 0, canvas.width, canvas.height);
 canvasContext.lineCap = 'round';
 canvasContext.strokeStyle = 'black';
-canvasContext.fillStyle = 'black';
 
 canvas.addEventListener('mousedown', (e) => {
     isPainting = true;
 });
 
 canvas.addEventListener('mouseup', (e) => {
+    isPainting = false;
+    canvasContext.stroke();
+    canvasContext.beginPath();
+});
+
+canvas.addEventListener('mouseleave', (e) => {
     isPainting = false;
     canvasContext.stroke();
     canvasContext.beginPath();
@@ -40,38 +43,129 @@ canvas.addEventListener('mousemove', (e) => {
     canvasContext.stroke();
 });
 
-let cleanCanvasButton = document.getElementById('algorithm-neural-network-clean-canvas-button');
-cleanCanvasButton.addEventListener('click', () => {
-    canvasContext.clearRect(0, 0, canvas.width, canvas.height);
-});
-
-let sendPictureButton = document.getElementById('algorithm-neural-network-send-picture-button');
-sendPictureButton.addEventListener('click', () => {
-    let image = canvasContext.getImageData(0, 0, canvas.width, canvas.height).data;
-    let fourthChannel = new Uint8Array(image.length / 4);
-    for (let i = 0; i < image.length; i += 4) {
-        fourthChannel[i / 4] = image[i + 3];
+class ImageEditor {
+    constructor() {
+        this.originalCanvas = canvas;
+        this.originalContext = canvasContext;
+        this.originalWidth = canvas.width;
+        this.originalHeight = canvas.height;
     }
 
-    let resizedArray = new Float32Array(28 * 28);
-    let scaleX = canvas.width / 28;
-    let scaleY = canvas.height / 28;
+    extractAlphaChannel(imageData) {
+        let alphaChannel = [];
+        for (let i = 0; i < imageData.length; i += 4) {
+            alphaChannel.push(data[i + 3]);
+        }
+        return alphaChannel;
+    }
 
-    for (let y = 0; y < 28; y++) {
-        for (let x = 0; x < 28; x++) {
-            let sum = 0;
-            let count = 0;
-            for (let yy = Math.floor(y * scaleY); yy < Math.ceil((y + 1) * scaleY); yy++) {
-                for (let xx = Math.floor(x * scaleX); xx < Math.ceil((x + 1) * scaleX); xx++) {
-                    sum += fourthChannel[yy * canvas.width + xx];
-                    count++;
+    arrayToMatrix(array, width, height) {
+        let matrix = [];
+        for (let y = 0; y < height; y++) {
+            matrix.push(array.slice(y * width, (y + 1) * width));
+        }
+        return matrix;
+    }
+
+    getBorderCoordinates() {
+        let minX = this.originalWidth,
+            minY = this.originalHeight,
+            maxX = 0,
+            maxY = 0;
+        for (let y = 0; y < this.originalHeight; y++) {
+            for (let x = 0; x < this.originalWidth; x++) {
+                if (matrixRepresentation[y][x] > 10) {
+                    minX = Math.min(minX, x);
+                    minY = Math.min(minY, y);
+                    maxX = Math.max(maxX, x);
+                    maxY = Math.max(maxY, y);
                 }
             }
-            resizedArray[y * 28 + x] = sum / count;
+        }
+        return [minX, minY, maxX, maxY];
+    }
+
+    centerImage(imageBorders) {
+        let imageWidth = imageBorders[2] - imageBorders[0];
+        let imageHeight = imageBorders[3] - imageBorders[1];
+
+        let biggerDimension = Math.max(imageWidth, imageHeight);
+        let smallerDimension = Math.min(imageWidth, imageHeight);
+        let sideLength = Math.floor(biggerDimension + biggerDimension / 4);
+
+        let tempCanvas = document.createElement('canvas');
+        let tempContext = tempCanvas.getContext('2d');
+        tempCanvas.width = tempCanvas.height = sideLength;
+
+        if (imageHeight > imageWidth) {
+            tempContext.putImageData(
+                this.originalContext.getImageData(
+                    imageBorders[0],
+                    imageBorders[1],
+                    imageWidth,
+                    imageHeight
+                ),
+                (sideLength - smallerDimension) / 2,
+                (sideLength - biggerDimension) / 2
+            );
+        } else {
+            tempContext.putImageData(
+                this.originalContext.getImageData(
+                    imageBorders[0],
+                    imageBorders[1],
+                    imageWidth,
+                    imageHeight
+                ),
+                (sideLength - biggerDimension) / 2,
+                (sideLength - smallerDimension) / 2
+            );
+        }
+        return [
+            this.extractAlphaChannel(tempContext.getImageData(0, 0, sideLength, sideLength).data),
+            sideLength,
+        ];
+    }
+
+    resizeImage(image, sideLength) {
+        let imageData = new Float32Array(28 * 28);
+        let scaleX = Math.floor(sideLength / 28);
+        let scaleY = Math.floor(sideLength / 28);
+        for (let y = 0; y < 28; y++) {
+            for (let x = 0; x < 28; x++) {
+                let sum = 0;
+                let count = 0;
+                for (let yy = Math.floor(y * scaleY); yy < Math.ceil((y + 1) * scaleY); yy++) {
+                    for (let xx = Math.floor(x * scaleX); xx < Math.ceil((x + 1) * scaleX); xx++) {
+                        sum += image[yy * sideLength + xx];
+                        count++;
+                    }
+                }
+                let avg = sum / count;
+                if (avg > 240) {
+                    avg -= 10;
+                }
+                imageData[y * 28 + x] = avg;
+            }
         }
     }
-    sendPicture(resizedArray);
-});
+
+    getImage() {
+        let originalImage = this.extractAlphaChannel(
+            canvasContext.getImageData(0, 0, canvas.width, canvas.height).data
+        );
+
+        let matrixRepresentation = this.arrayToMatrix(originalImage, originalWidth, originalHeight);
+        let imageBorders = this.getBorderCoordinates(matrixRepresentation);
+
+        if (imageBorders[0] >= imageBorders[2] || imageBorders[1] >= imageBorders[3]) {
+            alert('Please, draw a digit on the canvas first.');
+            return;
+        }
+
+        let centeredImage = this.centerImage(imageBorders);
+        return this.resizeImage(...centeredImage);
+    }
+}
 
 function sendPicture(resizedArray) {
     fetch('http://127.0.0.1:5000/recognize_digit', {
@@ -89,3 +183,14 @@ function sendPicture(resizedArray) {
         })
         .catch((error) => console.error('Error:', error));
 }
+
+let cleanCanvasButton = document.getElementById('algorithm-neural-network-clean-canvas-button');
+cleanCanvasButton.addEventListener('click', () => {
+    canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+let sendPictureButton = document.getElementById('algorithm-neural-network-send-picture-button');
+sendPictureButton.addEventListener('click', () => {
+    let imageEditor = new ImageEditor();
+    sendPicture(imageEditor.getImage());
+});
