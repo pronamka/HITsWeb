631eacbf999322ecf9165ca5e0cc6f2f4d35fbe9
pronamka <defender0508@gmail.com>
2025-04-16 12:49:03 +0700

    added a way to retrain model and save it

--- neural_network/server.py
+++ neural_network/server.py
@@ -9,20 +9,30 @@ app = Flask(__name__)
 CORS(app, origins="*")
 
 digits_recognizer = DigitsRecognizer(
-    network_structure=(784, 256, 128, 64, 10),
-    learning_rate=0.001,
-    dropout_rate=0.2,
-    l2_regularization_rate=0.01,
-    batch_size=64,
+    network_structure=(784, 256, 128, 10),
     initialize_from_existing_parameters=True
 )
+
+training_labels, training_data = digits_recognizer.load_data()
+
+
+@app.route('/save_model', methods=['GET'])
+def save_model():
+    digits_recognizer.save_model()
+    return "Success"
 
 
 @app.route('/recognize_digit', methods=['GET', 'POST'])
 def recognize_digit():
     data = request.get_json().get("image")
+    digit = request.get_json().get("digit")
     image = np.array(data) / 255
-    prediction_int = digits_recognizer.predict(image)[0]
+    if digit:
+        prediction_int = digits_recognizer.retrain(image, digit)[0]
+        digits_recognizer.retrain(training_data, training_labels)
+        print(digits_recognizer.predict(image, with_percentage=True))
+    else:
+        prediction_int = digits_recognizer.predict(image)[0]
     print(prediction_int)
     return jsonify({"digit": int(prediction_int)})
 
